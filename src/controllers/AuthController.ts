fb5c62b690917db4962a2afd1200e8e0d7dd3eb1
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2026-04-07 16:16:50 +0630

    Update authController

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -6,13 +6,17 @@ export const register = [
     .trim()
     .notEmpty()
     .matches("^[0-9]+$")
-    .isLength({ min: 5, max: 12 }),
+    .isLength({ min: 5, max: 12 })
+    .withMessage("Invalid phone number"),
   async (req: Request, res: Response, next: NextFunction) => {
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //   return res.status(400).json({ errors: errors.array() });
-    // }
-    res.render("404", { title: "404 Error", message: "Page Not Found" });
+    const errors = validationResult(req).array({onlyFirstError: true});
+    if (errors.length > 0) {
+      const error:any = new Error(errors[0]?.msg);
+      error.status = 400;
+      error.code = "VALIDATION_ERROR";
+      return next(error);
+    }
+     res.status(200).json({ message: "registered successfully" });
   },
 ];
 
