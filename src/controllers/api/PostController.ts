6c4e9a27c84ccbd6137c9874fd9e98670af33463
BoBoYanZawDev <boboyanzaw.dev@gmail.com>
2026-05-15 18:56:37 +0630

    update infinite scroll

--- src/controllers/api/PostController.ts
+++ src/controllers/api/PostController.ts
@@ -68,9 +68,9 @@ export const getPostsByPagination = [
 
     const page = req.query.page || 1;
     const limit = req.query.limit || 5;
-    const userId = req.userId;
-    const user = await getUserById(userId!);
-    checkUserIfNotExists(user);
+    // const userId = req.userId;
+    // const user = await getUserById(userId!);
+    // checkUserIfNotExists(user);
 
     const skip = (+page - 1) * +limit;
     const options = {
@@ -117,12 +117,62 @@ export const getPostsByPagination = [
   },
 ];
 
+// infinte scroll
 export const getInfinitePostsByPagination = [
+  query("cursor", "Page number must be unsigned integer.")
+    .isInt({ gt: 0 })
+    .optional(),
+  query("limit", "Limit number must be unsigned integer.")
+    .isInt({ gt: 4 })
+    .optional(),
   async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req).array({ onlyFirstError: true });
     if (errors.length > 0) {
       const error: any = createError(errors[0]?.msg, 400, errorCode.invalid);
       return next(error);
     }
+
+    const lastCursor = req.query.cursor;
+    const limit = req.query.limit || 5;
+    // const userId = req.userId;
+    // const user = await getUserById(userId!);
+    // checkUserIfNotExists(user);
+
+    const options = {
+      skip: lastCursor ? 1 : 0,
+      take: +limit + 1,
+      cursor: lastCursor ? { id: +lastCursor } : undefined,
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        body: true,
+        image: true,
+        updatedAt: true,
+        author: {
+          select: {
+            fullName: true,
+          },
+        },
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+    };
+
+    const posts = await getPostsList(options);
+
+    const hasNextPage = posts.length > +limit;
+
+    if (hasNextPage) {
+      posts.pop();
+    }
+    const newCursor = posts.length > 0 ? posts[posts.length - 1]?.id : null;
+    res.status(200).json({
+      message: "Get all infinite post.",
+      posts,
+      hasNextPage,
+      newCursor
+    });
   },
 ];
